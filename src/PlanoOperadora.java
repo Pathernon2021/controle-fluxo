70aa1041748871a5b306505ca2dbdb33257837c5
Pathernon2021 <santine.silva@bol.com.br>
2023-09-20 18:43:01 -0300

    Atualização

---
+++ src/PlanoOperadora.java
@@ -0,0 +1,19 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "T";
+
+        switch (plano){
+            case "T" :{
+                System.out.println("5G YouTube");
+            }
+            case "M" :{
+                System.out.println("Whats e Instagram Gratis");
+            }
+             case "B" :{
+                System.out.println("100 Minutos de Ligação ");
+        }
+
+        }
+}
+}
+
